dcdedeff64d3a183fd4f8eb39e424f035d03fcd6
8b8t-xyz <102479728+8b8t-xyz@users.noreply.github.com>
2022-03-27 19:38:20 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
                 <li>STORAGE: 2x8TB Datacenter grade HDDs</li>
                 <li>DDOS Protection: Cosmicguard</li>
                 <li>8b8t is a non-vanilla anarchy server with no rules which means everything is
-                    allowed and you can not get punished (No bans or mutes or kicks).
+                    allowed and you can not get punished.
                 </li>
             </ul>
 
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
                 Join the 8b8t.xyz Official Discord Server to keep up with the latest updates with the
                 server: <a href="https://discord.gg/bMG6tdwW" target="_blank"
                            className="link"
-                           rel="noreferrer">https://discord.gg/bMG6tdwW</a>
+                           rel="noreferrer">discord link</a>
             </p>
 
             <h2>Popular questions</h2>
@@ -31,13 +31,13 @@ const Home: NextPage = () => {
             <p>
                 8b8t is a Minecraft Anarchy Server, that means there are no rules. It&apos;s one of the
                 few
-                32k (also known as offline mode) anarchy servers which allows 32k weapons and illegals. The
-                server is semi-vanilla, it has commands like /tpa,and /nick aswell as /nc. 8b8t started January
-                16 2022 and the map will never reset.
+                32k meta anarchy servers which allows 32k weapons and illegals. The
+                server is semi-vanilla, it has commands like /tpa,and /nick aswell as /nc. 8b8t started March 17
+                2022 and the map will never reset.
             </p>
 
             <h3>What is 8b8t seed?</h3>
-            <p>6b6t seed is the same as 2b2t seed: -935168149848476023</p>
+            <p>8b8t seed is the same as the old seed: -935168149848476023</p>
 
             <h3>Is hacking allowed?</h3>
             <p>Yes, as mentioned above, there are truly no rules.</p>
@@ -45,7 +45,7 @@ const Home: NextPage = () => {
             <h3>Are lag machines allowed?</h3>
             <p>
                 Yes, the 8b8t admin has already patched most of the lag machines and if you help us find a
-                working one, we will reward you.
+                working one, he will reward you.
             </p>
         </Layout>
     )
